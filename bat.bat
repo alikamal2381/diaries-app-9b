a06dc741069eb9236891d6d3131b2c55a31f8715
AliKamal <alikamal2381@gmail.com>
2021-03-27 16:45:02 +0500

    batch file del

--- bat.bat
+++
@@ -1,7 +0,0 @@
-echo "# diaries-app-9b" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/alikamal2381/diaries-app-9b.git
-git push -u origin main
